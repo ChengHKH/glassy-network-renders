10b11eec90eee8536b2df961b2d401215298079f
unknown <ChengHKH@gmail.com>
2023-02-19 23:56:04 +0000

    implement custom bonding

--- pickle2pdb.py
+++ pickle2pdb.py
@@ -5,7 +5,7 @@ import pathlib
 import pickle
 
 
-from chemfiles import Topology, Frame, Atom, UnitCell, Trajectory
+from chemfiles import Frame, Atom, UnitCell, Trajectory, Selection
 
 
 def load_data(file):
@@ -13,16 +13,43 @@ def load_data(file):
         data = pickle.load(f)
     
     glass_structure = Frame()
-    glass_structure.cell = UnitCell(data['box'] / 3)
+    # glass_structure.cell = UnitCell(data['box'])
+    
     for i in range(4096):
-        if any(data['positions'][i] > data['box'] / 3):
+        if any(data['positions'][i] > data['box'] / 2):
             continue
 
         if data['types'][i] == 0:
-            glass_structure.add_atom(Atom("Si"), data['positions'][i])
+            glass_structure.add_atom(Atom("Ni"), data['positions'][i])
         elif data['types'][i] == 1:
-            glass_structure.add_atom(Atom("O"), data['positions'][i])
-    glass_structure.guess_bonds()
+            glass_structure.add_atom(Atom("P"), data['positions'][i])
+
+    AA = "two: name(#1) == Ni and name(#2) == Ni and " \
+        "distance(#1, #2) > 0.03 and distance(#1, #2) < (0.6 * 2.5 * 1)"
+    
+    AB = "two: name(#1) == Ni and name(#2) == P and " \
+        "distance(#1, #2) > 0.03 and distance(#1, #2) < (0.6 * 2.5 * 0.8)"
+
+    BB = "two: name(#1) == P and name(#2) == P and " \
+        "distance(#1, #2) > 0.03 and distance(#1, #2) < (0.6 * 2.5 * 0.88)"
+
+    selection = Selection(AA)
+    to_bond = selection.evaluate(glass_structure)
+    for pair in to_bond:
+        (i, j) = pair
+        glass_structure.add_bond(i, j)
+
+    selection = Selection(AB)
+    to_bond = selection.evaluate(glass_structure)
+    for pair in to_bond:
+        (i, j) = pair
+        glass_structure.add_bond(i, j)
+
+    selection = Selection(BB)
+    to_bond = selection.evaluate(glass_structure)
+    for pair in to_bond:
+        (i, j) = pair
+        glass_structure.add_bond(i, j)
 
     return glass_structure
 
